fb5cfdd16af5314db8cbeb7fa1e07389472f8991
osakac <shinmoriof@gmail.com>
2023-03-02 23:28:38 +0400

    added navbar categories dropdown

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,15 +1,43 @@
 <template>
   <nav class="navbar">
     <h2>Fake Store API</h2>
-    <ul>
-      <li><router-link to="/">Главная</router-link></li>
-      <li><router-link to="/categories">Категории</router-link></li>
+    <ul class="navbar__list">
+      <li class="navbar__item"><router-link to="/">Главная</router-link></li>
+      <li @mouseover="showDropdown" @mouseleave="hideDropdown" class="dropdown__trigger navbar__item">Категории
+        <div v-if="visibleDropdown" class="dropdown">
+          <ul class="dropdown__menu">
+            <li v-for="item in categories" :key="item" class="dropdown__item">{{ item }}</li>
+          </ul>
+        </div>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script>
+import { ref, onMounted, computed } from 'vue';
+import { useStore } from 'vuex';
+
 export default {
-  setup() { }
+  setup() {
+    const store = useStore()
+
+    const categories = computed(() => store.getters.getCategories)
+    const visibleDropdown = ref(false)
+
+    function showDropdown() {
+      visibleDropdown.value = true
+    }
+
+    function hideDropdown() {
+      visibleDropdown.value = false
+    }
+
+    onMounted(async () => {
+      await store.dispatch('loadCategories')
+    })
+
+    return { categories, showDropdown, hideDropdown, visibleDropdown }
+  }
 }
 </script>
