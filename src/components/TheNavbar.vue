53b6421d23ec109af48ed5266a3d400839bc0f86
osakac <shinmoriof@gmail.com>
2023-03-02 22:42:45 +0400

    first commit

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar">
+    <h2>Fake Store API</h2>
+    <ul>
+      <li><router-link to="/">Главная</router-link></li>
+      <li><router-link to="/categories">Категории</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  setup() { }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,48 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    products: [],
+    product: {},
+    categories: [],
   },
   getters: {
+    getProducts(state) {
+      return state.products;
+    },
+    getCategories(state) {
+      return state.categories;
+    },
   },
   mutations: {
+    setProducts(state, products) {
+      state.products = products;
+    },
+    setProduct(state, product) {
+      state.product = product;
+    },
+    setCategories(state, categories) {
+      state.categories = categories;
+    },
   },
   actions: {
+    async loadProducts({ commit }) {
+      const response = await fetch(`${URL}`);
+      const data = await response.json();
+      commit("setProducts", data);
+    },
+    async loadProduct({ commit }, id) {
+      const response = await fetch(`${URL}/${id}`);
+      const data = await response.json();
+      commit("setProduct", data);
+    },
+    async loadCategories({ commit }) {
+      const response = await fetch(`${URL}/categories`);
+      const data = await response.json();
+      commit("setCategories", data);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
+
+const URL = "https://fakestoreapi.com/products";
